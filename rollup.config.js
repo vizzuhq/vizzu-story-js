a58dabd52aaaf7791de94ca8d2d09e789e49785f
David Vegh <david.andras.vegh+github@gmail.com>
2022-07-06 17:59:33 +0200

    package: add rollup, pack and build script

---
+++ rollup.config.js
@@ -0,0 +1,17 @@
+import { terser } from "rollup-plugin-terser";
+
+const path = require('path');
+ 
+export default [
+    {
+        input: path.resolve(__dirname, './src/vizzu-player.js'),
+        output: {
+            file: path.resolve(__dirname, './dist/vizzu-story.min.js'),
+            format: 'es',
+            name: 'bundle'
+        },
+        plugins: [
+            terser()
+        ]
+    }
+]
