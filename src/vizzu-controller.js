49d98bafa76ceef1a0336edd8ebe2454ab61689b
dovicsin <tamas.dovicsin@gmail.com>
2023-08-16 22:03:22 +0200

    fixed: add opacity transition in host and fix webkit  point-envets cahange update fixed #27

--- src/vizzu-controller.js
+++ src/vizzu-controller.js
@@ -208,6 +208,7 @@ class VizzuController extends HTMLElement {
           --_bg: var(--vizzu-button-background-color, #fff);
           --_hc: var(--vizzu-button-hover-color, #494949);
           color: var(--_c);
+          transition: opacity 10ms;
         }
         :host([first]) #start,
         :host([first]) #previous,
