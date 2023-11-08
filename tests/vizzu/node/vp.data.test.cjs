e655e20c947e49c55da33db0b44ad8913f5e216c
dovicsin <tamas.dovicsin@gmail.com>
2023-11-08 09:42:00 +0100

    read cases out file

--- tests/vizzu/node/vp.data.test.cjs
+++ tests/vizzu/node/vp.data.test.cjs
@@ -1,8 +1,7 @@
 const puppeteer = require('puppeteer')
 const path = require('path')
 const express = require('express')
-const { excepted } = require('../../assets/data-cases/basic')
-
+const cases = require('../../assets/data-cases/index')
 
 describe('vizzu chart data', () => {
   let server
@@ -24,96 +23,60 @@ describe('vizzu chart data', () => {
     server.close()
   })
 
-  test('slide data test', async () => {
+  test.each(cases)(`slide data test with $name`, async ({ name, slides, excepted }) => {
     const page = await browser.newPage()
 
-    const vizzuPlayerData = {
-      data: {
-        series: [
-          {
-            name: 'Foo',
-            values: ['Alice', 'Bob', 'Ted']
-          },
-          {
-            name: 'Bar',
-            values: [15, 32, 12]
-          },
-          {
-            name: 'Baz',
-            values: [5, 3, 2]
-          }
-        ]
-      },
-      slides: [
-        [
-          {
-            config: {
-              x: 'Foo',
-              y: 'Bar',
-              label: 'Bar'
-            }
-          }
-        ],
-        [
-          {
-            config: {
-              color: 'Foo',
-              x: 'Baz',
-              label: 'Baz',
-              geometry: 'circle'
-            }
-          }
-        ]
-      ]
-    }
 
-    const excepted = '["15","32","12","5","3","2"]'
-
-    const keyFramesLengths = vizzuPlayerData.slides.map((slide) => slide.length)
+    const keyFramesLengths = slides.slides.map((slide) => slide.length)
     const allKeyFrames = keyFramesLengths.reduce((accumulator, currentValue) => {
       return accumulator + currentValue
     }, 0)
 
-    const data = await page.evaluate(async (serverPort, vizzuPlayerData, allKeyFrames) => {
-      document.body.innerHTML = '<vizzu-player id="story" controller></vizzu-player>'
+    const data = await page.evaluate(
+      async (serverPort, slides, allKeyFrames) => {
+        document.body.innerHTML = '<vizzu-player id="story" controller></vizzu-player>'
 
-      let resolveComplete = null
-      const completed = new Promise((resolve) => {
-        resolveComplete = resolve
-      })
+        let resolveComplete = null
+        const completed = new Promise((resolve) => {
+          resolveComplete = resolve
+        })
 
-      let completeCounter = 0
-      let datas = []
-      function labelDrawHandler(event) {
-        if (completeCounter < allKeyFrames) {
-          datas.push(event.data.text)
+        let completeCounter = 0
+        let datas = []
+        function labelDrawHandler(event) {
+          if (completeCounter < allKeyFrames) {
+            datas.push(event.data.text)
+          }
         }
-      }
 
-      const complete = () => {
-        if (completeCounter === allKeyFrames) {
-          resolveComplete(true)
+        const complete = () => {
+          if (completeCounter === allKeyFrames) {
+            resolveComplete(true)
+          }
+          completeCounter++
         }
-        completeCounter++
-      }
 
-      const VizzuPlayerModule = await import(`http://127.0.0.1:${serverPort}/src/vizzu-player.js`)
-      const VizzuPlayer = VizzuPlayerModule.default
-      const vp = document.getElementById('story')
-      window.vp = vp
-      vp.initializing.then(async (chart) => {
-        vp.style.cssText = 'width: 100%;height: 100%;'
-        chart.feature('tooltip', true)
-        chart.on('plot-marker-label-draw', labelDrawHandler)
-        chart.on('animation-complete', complete)
-        vp.slides = vizzuPlayerData;
-      })
+        const VizzuPlayerModule = await import(`http://127.0.0.1:${serverPort}/src/vizzu-player.js`)
+        const VizzuPlayer = VizzuPlayerModule.default
+        const vp = document.getElementById('story')
+        window.vp = vp
+        vp.initializing.then(async (chart) => {
+          vp.style.cssText = 'width: 100%;height: 100%;'
+          chart.feature('tooltip', true)
+          chart.on('plot-marker-label-draw', labelDrawHandler)
+          chart.on('animation-complete', complete)
+          vp.slides = slides
+        })
 
-      await completed
+        await completed
 
-      return datas
-    }, server.address().port, vizzuPlayerData, allKeyFrames)
+        return datas
+      },
+      server.address().port,
+      slides,
+      allKeyFrames
+    )
 
-    expect(JSON.stringify(data)).toStrictEqual(excepted)
+    expect(data).toStrictEqual(excepted)
   })
 })
