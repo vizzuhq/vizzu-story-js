c77bfd41fc924b428461b40a390b6a51850a1211
David Vegh <veghdev@gmail.com>
2025-01-30 13:39:59 +0100

    fix format

--- tests/vizzu-player/e2e/vp.data.label.test.cjs
+++ tests/vizzu-player/e2e/vp.data.label.test.cjs
@@ -23,7 +23,7 @@ describe('if slides set', () => {
 
 		browser = await puppeteer.launch({
 			headless: 'new',
-   			args: ['--disable-web-security', '--no-sandbox']
+			args: ['--disable-web-security', '--no-sandbox']
 		})
 	})
 
